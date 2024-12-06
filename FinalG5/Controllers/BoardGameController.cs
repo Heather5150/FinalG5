82278e0e8b533bec65eccc63f31bc80b6ad7b573
PlaceA101 <placean@mail.uc.edu>
2024-12-06 17:15:53 -0500

    Added Board Game controller and models

---
+++ FinalG5/Controllers/BoardGameController.cs
@@ -0,0 +1,74 @@
+﻿using FinalG5.Data;
+using FinalG5.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalG5.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BoardGamesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public BoardGamesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+       
+        [HttpGet]
+        public IActionResult GetBoardGames(int? id)
+        {
+            if (id == null || id == 0)
+                return Ok(_context.BoardGames.Take(5).ToList());
+
+            var BoardGames = _context.BoardGames.Find(id);
+            if (BoardGames == null) return NotFound();
+            return Ok(BoardGames);
+        }
+
+        [HttpPost]
+        public IActionResult CreateBoardGames([FromBody] BoardGames BoardGames)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            _context.BoardGames.Add(BoardGames);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetBoardGames), new { id = BoardGames.Id }, BoardGames);
+        }
+
+       
+        [HttpPut("{id}")]
+        public IActionResult UpdateBoardGames(int id, [FromBody]
+        BoardGames BoardGames)
+        {
+            if (id != BoardGames.Id) return BadRequest();
+
+            var existingBoardGames = _context.BoardGames.Find(id);
+            if (existingBoardGames == null) return NotFound();
+
+            existingBoardGames.Name = BoardGames.Name;
+            existingBoardGames.Description = BoardGames.Description;
+            existingBoardGames.DifficultyLevel = BoardGames.DifficultyLevel;
+            existingBoardGames.AveragePlayTime = BoardGames.AveragePlayTime;
+
+            _context.BoardGames.Update(existingBoardGames);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+       
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBoardGames(int id)
+        {
+            var BoardGames = _context.BoardGames.Find(id);
+            if (BoardGames == null) return NotFound();
+
+            _context.BoardGames.Remove(BoardGames);
+            _context.SaveChanges();
+            return NoContent();
+        }
+    }
+}
--- FinalG5/Data/AppDbContext.cs
+++ FinalG5/Data/AppDbContext.cs
@@ -9,6 +9,7 @@ namespace FinalG5.Data
     {
         public DbSet<TeamMember> TeamMembers { get; set; }
         public DbSet<Hobby> Hobbies { get; set; }
+        public DbSet<BoardGames> BoardGames { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Hobby>().HasData(
@@ -16,7 +17,13 @@ namespace FinalG5.Data
                 new Hobby { Id = 2, Name = "Painting", Description = "Creating art using paint", DifficultyLevel = 4, AverageTimeSpentPerWeek = 3.0, IsOutdoorActivity = false },
                 new Hobby { Id = 3, Name = "Hiking", Description = "Walking trails in nature", DifficultyLevel = 4, AverageTimeSpentPerWeek = 6.0, IsOutdoorActivity = true }
             );
+            modelBuilder.Entity<BoardGames>().HasData(
+               new BoardGames { Id = 1, Name = "Uno", Description = "Playing till no more cards in hand", DifficultyLevel = 1, AveragePlayTime = 0.15},
+               new BoardGames { Id = 2, Name = "Monopoly", Description = "Buy all the properties you can and bankrupt your friends", DifficultyLevel = 3, AveragePlayTime = 3.5},
+               new BoardGames { Id = 3, Name = "Dungeons and Dragons", Description = "Role playing board game to play with friends", DifficultyLevel = 5, AveragePlayTime = 8760.0}
+           );
         }
+
 
     }
 }
