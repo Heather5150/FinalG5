adb5e94f1534ae954188be9c0a54c5e9ad358b3c
Cage <wellmacj@mail.uc.edu>
2024-12-06 17:40:27 -0500

    added appDbContext info

--- FinalG5/Data/AppDbContext.cs
+++ FinalG5/Data/AppDbContext.cs
@@ -18,6 +18,11 @@ namespace FinalG5.Data
                 new Hobby { Id = 2, Name = "Painting", Description = "Creating art using paint", DifficultyLevel = 4, AverageTimeSpentPerWeek = 3.0, IsOutdoorActivity = false },
                 new Hobby { Id = 3, Name = "Hiking", Description = "Walking trails in nature", DifficultyLevel = 4, AverageTimeSpentPerWeek = 6.0, IsOutdoorActivity = true }
             );
+            modelBuilder.Entity<Exercise>().HasData(
+                new Exercise { Id = 1, Name = "Pushups", Description = "Using your arms to move your body", Repetitions = 10, Sets = 3, RequiresEquipment = false },
+                new Exercise { Id = 2, Name = "Squats", Description = "Squatting with legs", Repetitions = 8, Sets = 2, RequiresEquipment = false },
+                new Exercise { Id = 3, Name = "BicepCurl", Description = "Curling dumbbells for biceps", Repetitions = 12, Sets = 4, RequiresEquipment = true }
+            );
         }
 
     }
